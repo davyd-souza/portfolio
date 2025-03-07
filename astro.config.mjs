e283d91d143efc1f26a7b028df0fd24095666b01
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2025-03-06 23:36:50 -0300

    build: implement astro-icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,16 @@
 import { defineConfig } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
+import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
-	},
+    vite: {
+        plugins: [tailwindcss()],
+    },
+
+    integrations: [icon({
+        iconDir: 'src/assets/icons'
+    })],
 })
+
